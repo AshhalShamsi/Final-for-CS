b48fb1acb7e5b4677ab607a15fcc568338adda8d
AshhalShamsi <AAShamsi@student.naperville203.org>
2017-05-17 15:10:32 -0500

    sweg

--- final/mainGame.java
+++ final/mainGame.java
@@ -11,27 +11,26 @@ import java.util.*;
 public class mainGame
 {
     // instance variables - replace the example below with your own
-    private Env env;
+    private static Env env;
     private Room room;
     private Objc stuff, stuff2, fidget, test;
     private Ball ball1;
+    private Thread t = new Thread();
+    private final static double step = .15; 
     /**
      * Constructor for objects of class mainGame
      */
     public mainGame()
     {
-        room = new Room (15,10,10, "Mi cabeza es ROJO");
+        room = new Room (45,10,35, "Mi cabeza es ROJO");
         room.setTextureEast("textures/bricks.jpg");
         room.setTextureNorth("textures/bricks.jpg");
         room.setTextureSouth("textures/bricks.jpg");
         room.setTextureWest("textures/bricks.jpg");
         room.setTextureTop("textures/bricks.jpg");
         room.setTextureBottom("textures/bricks.jpg");
-        stuff = new Objc("models/test/test.jpg", "models/test/test.obj", 10,10,10);
         stuff2 = new Objc("models/test/test.jpg","models/trash/trash.obj",5,8,5);
         ball1 = new Ball(512, "models/test/test.jpg", 5, 2, 8, 1 );
-        fidget = new Objc("models/test/test.jpg","models/fidget/fidget.obj",5,2,5);
-        
         test = new Intensity_1(13, 13, 13);
     }
 
@@ -41,32 +40,85 @@ public class mainGame
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public void play()
+    public void play() throws InterruptedException
     {
         // put your code here
         // Position the camera
         env = new Env();
         boolean finished = false;
-        fidget.setscale(1);
         room.setCurrentRoom(env);
         env.setCameraXYZ(5, 13, 9);        
         System.out.println(env.getCameraPitch());
         // Disable mouse and camera control
-        env.setDefaultControl(true);
-        stuff.setRoomDim(room.getWidth(), room.getDepth());     
-        env.addObject(stuff);
+        env.setDefaultControl(finished);
         env.addObject(stuff2);
         env.addObject(ball1);
         env.addObject(test);
-        //env.addObject(fidget);
+       
         while (env.getKey() != 1)
         {
             ball1.setxyz(env.getCameraX()+(5*Math.cos(env.getCameraPitch()*Math.PI/180)*Math.sin(env.getCameraYaw()*Math.PI/180)), env.getCameraY()+(5*Math.sin(env.getCameraPitch()*Math.PI/180)), env.getCameraZ()+(5*Math.cos(env.getCameraPitch()*Math.PI/180) * Math.cos(env.getCameraYaw()*Math.PI/180)));
             //ball1.setYaw(env.getCameraYaw());
             env.advanceOneFrame();
             // Position the camera
-            
+            if(env.getKey() == 60)
+                if (finished == false)
+                    finished = true;
+                else
+                    finished = false;
+                    
+            if (finished == false)
+                move();
+            env.setDefaultControl(finished);
         }
         env.exit();
+    }
+    
+    
+    public static void move(){
+        
+                
+                if (env.getKeyDown(Keyboard.KEY_W)) {
+            env.setCameraXYZ(env.getCameraX()-step*Math.sin(Math.toRadians(env.getCameraYaw())),
+                                env.getCameraY(),
+                                env.getCameraZ()-step*Math.cos(Math.toRadians(env.getCameraYaw())));
+        }
+         
+        if (env.getKeyDown(Keyboard.KEY_S)) {
+            env.setCameraXYZ(env.getCameraX()+step*Math.sin(Math.toRadians(env.getCameraYaw())),
+                                env.getCameraY(),
+                                env.getCameraZ()+step*Math.cos(Math.toRadians(env.getCameraYaw())));
+        }
+         
+        if (env.getKeyDown(Keyboard.KEY_A)) {
+            env.setCameraXYZ(env.getCameraX()-step*Math.cos(Math.toRadians(env.getCameraYaw())),
+                                env.getCameraY(),
+                                env.getCameraZ()+step*Math.sin(Math.toRadians(env.getCameraYaw())));
+        }
+        if (env.getKeyDown(Keyboard.KEY_D)) {
+            env.setCameraXYZ(env.getCameraX()+step*Math.cos(Math.toRadians(env.getCameraYaw())),
+                                env.getCameraY(),
+                                env.getCameraZ()-step*Math.sin(Math.toRadians(env.getCameraYaw())));
+        }
+         
+        double newPitch = env.getCameraPitch()+env.getMouseDY();
+         
+        // Restrict the pitch to within a "reasonable" amount
+        if (newPitch > 50) {
+            env.setCameraPitch(50);
+        } else if (newPitch < -50) {
+            env.setCameraPitch(-50);
+        } else {
+            env.setCameraPitch(newPitch);
+        }
+         
+        env.setCameraYaw(env.getCameraYaw()-env.getMouseDX());
+
+}
+    
+    
+public static void main(String [] args) throws InterruptedException
+    {  
+        (new mainGame()).play();
     }
 }
